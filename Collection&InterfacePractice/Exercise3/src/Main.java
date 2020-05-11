4c9ff3e1f56a7ba85e3d0ba9195decf58c76d447
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-05-11 17:53:23 -0300

    [Fix] Exercise 3, removeEvenLength method

--- Collection&InterfacePractice/Exercise3/src/Main.java
+++ Collection&InterfacePractice/Exercise3/src/Main.java
@@ -1,25 +1,31 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         list.add("aaaa");
-        list.add("123");
-        list.add("C");
+        list.add("aaaa");
+        list.add("b");
         System.out.println(list);
         removeEvenLength(list);
         System.out.println(list);
     }
 
-    public static void removeEvenLength(ArrayList<String> list){
+    public static void removeEvenLength(List<String> list){
 
+        List<String> deletedStrings = new ArrayList<>();
         for (int i = 0; i < list.size(); i++){
 
             if(list.get(i).length()%2 == 0){
-                list.remove(i);
+                deletedStrings.add(list.get(i));
             }
+        }
+
+        for (String deleted : deletedStrings){
+            list.remove(deleted);
         }
     }
 }
