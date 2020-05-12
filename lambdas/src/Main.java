eca06d03ebdc64bb9479430a83f9fa4541d4c9d0
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-05-12 11:41:22 -0300

    [Exercise 1] finished

--- lambdas/src/Main.java
+++ lambdas/src/Main.java
@@ -1,6 +1,12 @@
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+
+        Function<Integer,Integer> function = number -> number%2;
+
+        System.out.println(function.apply(5) + "\n");
+
     }
 }
