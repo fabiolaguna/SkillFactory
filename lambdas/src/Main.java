5ee53949f09766154b18c8bc2cef6de1c776f5c6
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-05-12 11:55:22 -0300

    [Exercise 2] finished

--- lambdas/src/Main.java
+++ lambdas/src/Main.java
@@ -1,12 +1,16 @@
+import com.sun.jmx.snmp.Timestamp;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Function<Integer,Integer> function = number -> number%2;
-
         System.out.println(function.apply(5) + "\n");
+
+        Supplier<Timestamp> supplier = () -> new Timestamp(System.currentTimeMillis());
+        System.out.println(supplier.get() + "\n");
 
     }
 }
