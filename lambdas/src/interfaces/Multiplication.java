baecfc0956a791a738cb932d908db9dcf161c222
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-05-14 01:22:15 -0300

    [Refactor] mult to getMultiplication, method name in Multiplication

--- lambdas/src/interfaces/Multiplication.java
+++ lambdas/src/interfaces/Multiplication.java
@@ -3,5 +3,5 @@ package interfaces;
 @FunctionalInterface
 public interface Multiplication<T extends Number, V extends Number, R extends Number> {
 
-    R mult(T t, V v);
+    R getMultiplication(T t, V v);
 }
